a6d07ccab67f57ad8cbad9e6594d9f7fb2efb3bf
준희 <113875098+sungsil0624@users.noreply.github.com>
2023-09-26 18:26:46 +0900

    feat : add swarm
    군집 비행 코드 추가

--- drone/Assignment_#3/Swarm.py
+++ drone/Assignment_#3/Swarm.py
@@ -1,2 +1,18 @@
 from djitellopy import TelloSwarm
 
+swarm = TelloSwarm.fromIps([
+    "192.168.1.104",
+    "192.168.1.105"
+])
+
+swarm.connect()
+swarm.takeoff()
+
+# run in parallel on all tellos
+swarm.move_up(100)
+
+# run by one tello after the other
+swarm.sequential(lambda i, tello: tello.move_forward(i * 20 + 20))
+
+swarm.land()
+swarm.end()
