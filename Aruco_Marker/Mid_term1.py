a28df6240efe2faf32b79900daebcadd65c3c4b5
준희 <113875098+sungsil0624@users.noreply.github.com>
2023-10-16 14:17:47 +0900

    refactor : modify mid_term code
    이동 거리 수정

--- Aruco_Marker/Mid_term1.py
+++ Aruco_Marker/Mid_term1.py
@@ -4,11 +4,9 @@ from droneblocksutils.aruco_utils import detect_markers_in_image
 import time
 
 # 아르코 마커 관련 설정
-marker_size = 100  # 아르코 마커의 크기 (mm)
 marker_id_1 = 0  # 첫 번째 아르코 마커의 ID
 marker_id_2 = 1  # 두 번째 아르코 마커의 ID
 marker_id_3 = 2  # 세 번째 아르코 마커의 ID
-distance_to_marker = 30  # 아르코 마커 앞으로 전진할 거리 (센티미터)
 
 # Tello 드론 관련 설정
 tello = Tello()
@@ -17,12 +15,12 @@ tello.streamon()
 tello.takeoff()
 
 
-# Tello 드론 앞으로 전진 함수
+# 드론 앞으로 전진 함수
 def move_forward(distance_cm):
     tello.move_forward(distance_cm)  # 거리는 센티미터 단위로 지정
 
 
-# Tello 드론 왼쪽으로 회전 함수
+# 드론 왼쪽으로 회전 함수
 def rotate_left(degrees):
     tello.rotate_counter_clockwise(degrees)  # 시계 반대 방향으로 회전
 
@@ -32,7 +30,7 @@ try:
     state = "find_marker_1"
 
     while True:
-        # Tello의 비디오 프레임을 가져옵니다.
+        # 비디오 프레임 가져오기
         frame = tello.get_frame_read().frame
 
         # 아르코 마커 검출
@@ -45,8 +43,8 @@ try:
                 if detected_marker_id == 0:
                     # 첫 번째 아르코 마커를 발견했을 때
                     # marker_center_x, marker_center_y = marker_details[0][0], marker_details[0][1]
-
-                    move_forward(distance_to_marker)
+                    print(marker_details)
+                    move_forward(245)
                     rotate_left(90)
                     state = "find_marker_2"
 
@@ -54,22 +52,25 @@ try:
             if marker_details:
                 detected_marker_id = marker_details[0][1]
                 if detected_marker_id == 1:
-                    # 첫 번째 아르코 마커를 발견했을 때
+                    # 두번 째 아르코 마커를 발견했을 때
                     # marker_center_x, marker_center_y = marker_details[0][0], marker_details[0][1]
-
-                    move_forward(distance_to_marker)
-                    rotate_left(90)
+                    print(marker_details)
+                    move_forward(230)
+                    rotate_left(130)
                     state = "find_marker_3"
 
         elif state == "find_marker_3":
             if marker_details:
                 detected_marker_id = marker_details[0][1]
                 if detected_marker_id == 2:
-                    # 첫 번째 아르코 마커를 발견했을 때
+                    # 세번 째 아르코 마커를 발견했을 때
                     # marker_center_x, marker_center_y = marker_details[0][0], marker_details[0][1]
+                    print(marker_details)
                     state = "find_marker_4"
+                    move_forward(350)
                     tello.rotate_counter_clockwise(360)
         elif state == "find_marker_4":
+            tello.land()
             break
 
         # 비디오 화면에 아르코 마커 표시
@@ -81,8 +82,6 @@ try:
 
 except KeyboardInterrupt:
     pass
-
-tello.land()
 
 # Tello 드론 연결 해제
 tello.streamoff()
